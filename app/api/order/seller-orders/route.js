ef06007f31468b65ebc4489370a0c5d7664191a2
Vitaly Kolodchenko <kolodchenkovitaly250@gmail.com>
2025-09-09 00:33:51 +0300

    final

--- app/api/order/seller-orders/route.js
+++ app/api/order/seller-orders/route.js
@@ -1,21 +1,24 @@
 import connectDB from '@/config/db'
+import authSeller from '@/lib/authSeller'
 import Address from '@/models/Address'
 import Order from '@/models/Order'
-import Product from '@/models/Product'
 import { getAuth } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 export async function GET(request) {
 	try {
 		const { userId } = getAuth(request)
+		const isSeller = await authSeller(userId)
+
+		if (!isSeller) {
+			return NextResponse.json({ success: false, message: 'Not authorized' })
+		}
+
 		await connectDB()
 
 		Address.length
-		Product.length
 
-		const orders = await Order.find({ userId }).populate(
-			'address items.product'
-		)
+		const orders = await Order.find({}).populate('address items.product')
 
 		return NextResponse.json({ success: true, orders })
 	} catch (error) {
