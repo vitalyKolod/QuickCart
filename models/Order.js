7a3f6f296d71c9a2403f762c5757aca59ed93ed5
Vitaly Kolodchenko <kolodchenkovitaly250@gmail.com>
2025-09-08 19:02:56 +0300

    date->data

--- models/Order.js
+++ models/Order.js
@@ -11,7 +11,7 @@ const OrderSchema = new mongoose.Schema({
 	amount: { type: Number, required: true },
 	address: { type: String, ref: 'address', required: true },
 	status: { type: String, required: true, default: 'Order Placed' },
-	data: { type: Number, required: true },
+	date: { type: Number, required: true },
 })
 
 const Order = mongoose.models.order || mongoose.model('order', OrderSchema)
